9e27be6dcbf2ec1011a82f3b8d5adc0c10dacd3e
Santiago Ocamica <santi6982@gmail.com>
2015-04-22 17:44:57 -0300

    Implementación base de multithreading (comentada)

--- GC/src/model/RayTracer.java
+++ GC/src/model/RayTracer.java
@@ -1,6 +1,11 @@
 package model;
 
 import java.awt.image.BufferedImage;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 import javax.vecmath.Color3f;
 import javax.vecmath.Vector3d;
@@ -14,6 +19,7 @@ public class RayTracer {
 	private double pixelSize = 0.001;
 	
 	public RayTracer(Scene scene) {
+//		this.executor = Executors.
 		this.scene = scene;
 	}
 	
@@ -24,15 +30,25 @@ public class RayTracer {
 	}
 	
 	public BufferedImage render() {
+//		List<CompletableFuture<?>> futures = new LinkedList<CompletableFuture<?>>();
 		BufferedImage bi = new BufferedImage(scene.getCamera().getXRes(), scene.getCamera().getYRes(), BufferedImage.TYPE_INT_RGB);
 		setUp();
 		for (int i = 0; i < scene.getCamera().getXRes(); i++) {
 			for (int j = 0; j < scene.getCamera().getYRes(); j++) {
 				Ray ray = rayThroughPixel(i, j);
-				Color3f color = ray.trace(scene.getObjects()).shade(scene.getLights(), scene.getObjects());
-				bi.setRGB(i, j, color.get().getRGB());
+				Supplier<Color3f> s = ()-> ray.trace(scene.getObjects()).shade(scene.getLights(), scene.getObjects());
+				setColor(i, j, bi).accept(s.get());
+//				futures.add(tracing);
+//				futures.add(tracing.thenAccept(setColor(i, j, bi)));
 			}
 		}
+//		for (CompletableFuture<?> future : futures) {
+//			try {
+//				future.get();
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			};
+//		}
 		return bi;
 	}
 
@@ -41,5 +57,10 @@ public class RayTracer {
 		double y = pixelSize*(j - 0.5*scene.getCamera().getYRes());
 		Vector3d direction = normalize(sub(add(scale(u, x), scale(v, y)), scale(w, scene.getCamera().getDistanceToCamera())));
 		return new Ray(direction, scene.getCamera().getPosition());
+	}
+	private Consumer<Color3f> setColor(int i, int j, BufferedImage bi) {
+		return (color) -> {
+			bi.setRGB(i, j, color.get().getRGB());	
+		};
 	}
 }
