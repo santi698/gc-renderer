84dcfe91b33f1b5b449c53f23b50c6f494427493
Zeitzen <zeitzendesign@gmail.com>
2015-05-25 21:44:50 -0300

    bbox hit bix

--- GC/src/model/shapes/BoundingBox.java
+++ GC/src/model/shapes/BoundingBox.java
@@ -111,28 +111,28 @@ public class BoundingBox extends Shape {
 		
 		if(tx_min > ty_min){
 			t0 = tx_min;
-			face_in = (a >= 0.0 ) ? 0 : 3 ;
+			face_in = (a >= 0.0d ) ? 0 : 3 ;
 		}else{
 			t0 = ty_min;
-			face_in = (b >= 0.0 ) ? 1 : 4 ;
+			face_in = (b >= 0.0d ) ? 1 : 4 ;
 		}
 		
 		if( tz_min > t0){
 			t0 = tz_min;
-			face_in = (c >= 0.0 ) ? 2 : 5 ;
+			face_in = (c >= 0.0d ) ? 2 : 5 ;
 		}
 		
-		if(tz_max < ty_max){
-			t1 = tz_max;
-			face_out = (a >= 0.0 ) ? 3 : 0 ;
+		if(tx_max < ty_max){
+			t1 = tx_max;
+			face_out = (a >= 0.0d ) ? 3 : 0 ;
 		}else{
 			t1 = ty_max;
-			face_out = (b >= 0.0 ) ? 4 : 1 ;
+			face_out = (b >= 0.0d ) ? 4 : 1 ;
 		}
 		
 		if( tz_max < t1){
 			t1 = tz_max;
-			face_out = (c >= 0.0 ) ? 5 : 2 ;
+			face_out = (c >= 0.0d ) ? 5 : 2 ;
 		}
 		
 		double tmin = 0.0d;
